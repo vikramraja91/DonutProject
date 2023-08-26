43ff6c426337031603fd4fe8f7cb75014be73dda
vikram <vikramraja2009@gmail.com>
2023-08-26 20:44:00 +0530

    added exemption handle

--- src/main/scala/Donut.scala
+++ src/main/scala/Donut.scala
@@ -1,28 +1,37 @@
-class Donut(name:String="",price:Int=0) {
+class Donut(name: String = "", price: Int = 0) {
   var a = name
   var b = price
 
-
   def discount: Double = {
-    val discountedPrice = a match {
-      case "plain"    => b * 0.5
-      case "chocolate" => b * 1.5
-      case _          => b.toDouble
-    }
-
-    // Ensure that the discount is non-negative
-    if (discountedPrice < 0) {
-      0
-    } else {
-      discountedPrice
+    try {
+      if (b < 0) {
+        throw new IllegalArgumentException("Price cannot be negative")
+      }
+      a match {
+        case "plain" => b * 0.5
+        case "chocolate" => b * 1.5
+        case _ => b.toDouble
+      }
+    } catch {
+      case e: NumberFormatException =>
+        println(s"Invalid price of $a: $b")
+        0
+      case _: IllegalArgumentException =>
+        println("Price cannot be negative")
+        0
+      case _: Throwable =>
+        println("An unexpected error occurred")
+        0
     }
   }
-  override def toString: String = s"$name which has $price had a discount is $discount)"
 
+  override def toString: String = s"$name which has $price had a discount is $discount"
 }
 
-object Donut{
-  def apply(name:String="",price:Int=0):Donut={
-    new Donut(name,price)
+object Donut {
+  def apply(name: String = "", price: Int = 0): Donut = {
+    new Donut(name, price)
   }
 }
+
+
--- src/main/scala/DonutE.scala
+++ src/main/scala/DonutE.scala
@@ -1,11 +1,10 @@
 object DonutE {
-  def main(args:Array[String]):Unit={
-    val a=Donut("choclate",30)
-    val b=Donut("plain",10)
-    val c=Donut("straw",-5)
+  def main(args: Array[String]): Unit = {
+    val a = Donut("chocolate", 30)
+    val b = Donut("plain", 10)
+    val c = Donut("straw", -5)
     println(a)
     println(b)
     println(c)
   }
-
 }
